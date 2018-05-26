a5d3882b2a6d378bc2587d42a2ad02ff22748d0e
James <createdormade@gmail.com>
2018-05-26 15:59:31 -0700

    Added mutation

--- src/neurality/NeuralMap.java
+++ src/neurality/NeuralMap.java
@@ -98,7 +98,82 @@ public class NeuralMap {
 		connections = template.connections;
 		
 		for(int i = 0; i < radiation; i++){
-			//if()
+			int x = (int) Math.round(Math.random()*width);
+			if(x == width) {
+				int y = (int) Math.round(Math.random()*(outputs-1));
+				double s = Math.random();
+				if(s < 0.5) {
+					int[][] inputs = outputConnections[y];
+					int[][] temp = inputs;
+					inputs = new int[inputs.length+1][2];
+					for(int k = 0; k < temp.length; k++) inputs[k] = temp[k];
+					int ix = (int) Math.round(Math.random()*width);
+					int iy;
+					if(ix == 0) iy = (int) Math.round(Math.random()*(this.inputs-1));
+					else iy = (int) Math.round(Math.random()*(this.height-1));
+					inputs[inputs.length-1][0] = ix;
+					inputs[inputs.length-1][1] = iy;
+					outputConnections[y] = inputs;
+				} else {
+					int[][] inputs = outputConnections[y];
+					int[][] temp = inputs;
+					int skip = (int) Math.round(Math.random()*(inputs.length-1));
+					inputs = new int[inputs.length-1][2];
+					for(int k = 0; k < inputs.length; k++) {
+						if(k >= skip) inputs[k] = temp[k+1];
+						else inputs[k] = temp[k];
+					}
+					outputConnections[y] = inputs;
+				}
+			} else {
+				int y = (int) Math.round(Math.random()*(height-1));
+				double s = Math.random();
+				if(s < 0.25) {
+					NeuronWrapper[] register = registry.getWrappers();
+					NeuronWrapper wrapper = register[(int) Math.round(Math.random()*(register.length-1))];
+					map[x][y] = wrapper;
+					
+					mods[x][y] = new double[wrapper.modCount];
+					for(int ii = 0; ii < wrapper.modCount; ii++) mods[x][y][ii] = wrapper.minModValue + (Math.random()*(wrapper.maxModValue-wrapper.minModValue));
+					
+					int inputCount = 0;
+					while(Math.random() > registry.getComplexity()) inputCount++;
+					connections[x][y] = new int[inputCount][2];
+					for(int ii = 0; ii < inputCount; ii++){
+						int kx = (int) Math.round(Math.random() * x);
+						int ky;
+						if(kx == 0) ky = (int) Math.round(Math.random() * (inputs-1));
+						else ky = (int) Math.round(Math.random() * (height-1));
+						connections[x][y][ii][0] = kx;
+						connections[x][y][ii][1] = ky;
+					}
+				} else if(s < 0.5) {
+					int[][] inputs = connections[x][y];
+					int[][] temp = inputs;
+					inputs = new int[inputs.length+1][2];
+					for(int k = 0; k < temp.length; k++) inputs[k] = temp[k];
+					int ix = (int) Math.round(Math.random()*x);
+					int iy;
+					if(ix == 0) iy = (int) Math.round(Math.random()*(this.inputs-1));
+					else iy = (int) Math.round(Math.random()*(this.height-1));
+					inputs[inputs.length-1][0] = ix;
+					inputs[inputs.length-1][1] = iy;
+					connections[x][y] = inputs;
+				} else if(s < 0.75) {
+					int[][] inputs = connections[x][y];
+					int[][] temp = inputs;
+					int skip = (int) Math.round(Math.random()*(inputs.length-1));
+					inputs = new int[inputs.length-1][2];
+					for(int k = 0; k < inputs.length; k++) {
+						if(k >= skip) inputs[k] = temp[k+1];
+						else inputs[k] = temp[k];
+					}
+					connections[x][y] = inputs;
+				} else {
+					int target = (int) Math.round(Math.random()*(mods[x][y].length-1));
+					mods[x][y][target] = map[x][y].minModValue + (map[x][y].maxModValue - map[x][y].minModValue)*Math.random();
+				}
+			}
 		}
 	}
 	
